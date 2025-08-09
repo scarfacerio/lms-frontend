4ea22f304ba036393a7d47a3a5a86c250a166bd0
Muhammad Ahmad <itxscarface@gmail.com>
2025-08-09 07:45:02 +0500

    Fix: correct all changes

--- src/pages/auth/LogIn.jsx
+++ src/pages/auth/LogIn.jsx
@@ -4,10 +4,10 @@ import { BsEnvelope, BsLock } from "react-icons/bs"
 import { useDispatch } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 
-import option2 from '../../assets/Json/option2.json'
+import option2 from '../../assets/json/option2.json'
 import Particle from "../../components/Particle"
-import HomeLayout from "../../layouts/HomeLayout"
-import { forgotPassword, login } from "../../redux/slices/AuthSlice"
+import HomeLayout from "../../Layouts/HomeLayout"
+import { forgotPassword, login } from "../../Redux/slices/AuthSlice"
 function LogIn() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
